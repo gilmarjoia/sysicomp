f796bc60db3b481bbff609d99e978e30526403fa
Eduardo Pinheiro do Nascimento <pneduardo@gmail.com>
2017-09-28 23:53:01 -0400

    CRUD do módulo de ocorrências

---
+++ backend/views/ocorrencias/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Ocorrencias */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="ocorrencias-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'id')->textInput() ?>
+
+    <?= $form->field($model, 'codigo')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'ocorrencia')->textarea(['rows' => 6]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ backend/views/ocorrencias/create.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Ocorrencias */
+
+$this->title = 'Create Ocorrencias';
+$this->params['breadcrumbs'][] = ['label' => 'Ocorrencias', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="ocorrencias-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
