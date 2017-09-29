fbaaa522db35d15ac44eda72a59b367ae3791286
JFDValente <jfdv@icomp.ufam.edu.br>
2017-09-29 18:55:52 -0400

    ajusta na tela inicial de ocorrencias

--- backend/views/ocorrencias/index.php
+++ backend/views/ocorrencias/index.php
@@ -7,16 +7,15 @@ use yii\grid\GridView;
 /* @var $searchModel app\models\OcorrenciasSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Ocorrencias';
+$this->title = 'Ocorrências';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="ocorrencias-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Nova Ocorrencia', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Nova Ocorrência', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
