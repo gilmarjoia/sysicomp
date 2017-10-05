297c8cfcc71ef9ec9d80ed813e46fffe5754e494
Eduardo Pinheiro do Nascimento <pneduardo@gmail.com>
2017-10-05 16:43:37 -0400

    Estória 9 - Registrar frequências dos professores
    Index relativo as frequências e detalhar frequências.

---
+++ backend/views/frequencias/detalhar.php
@@ -0,0 +1,117 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\FrequenciasSearchSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Detalhes de Frequências';
+
+$this->params['breadcrumbs'][] = ['label' => 'Frequências', 'url' => ['listartodos',  "ano" => $_GET["ano"] ]];
+$this->params['breadcrumbs'][] = $this->title;
+
+
+
+if( isset($_GET["ano"]) && isset($_GET["prof"]) ){
+    $anoVoltar = $_GET["ano"];
+    $profVoltar = $_GET["prof"];
+}
+
+?>
+
+
+
+<?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+<p>
+
+    <?= Html::a('<span class="glyphicon glyphicon-arrow-left"></span> Voltar  ', ['listartodos', "ano" => $anoVoltar ], ['class' => 'btn btn-warning']) ?>
+    <?= Html::a('Registrar Novas Frequências', ['createsecretaria' , "id" => $id, "ano" => $anoVoltar , "prof" => $profVoltar ], ['class' => 'btn btn-success']) ?>
+</p>
+
+<?= DetailView::widget([
+    'model' => $model_do_usuario,
+    'attributes' => [
+
+
+        [
+            'attribute' => 'nome',
+            'label' => 'Nome',
+        ],
+
+        [
+            'attribute' => 'categoria',
+            'label' => 'Categoria',
+            'value' => $profVoltar == 1 ? "Professor" : "Secretaria"
+        ],
+
+        [
+            'attribute' => 'diasPagar',
+            'label' => 'Quantidade de Dias a Pagar:',
+            'value'=> 1,
+
+        ],
+        [
+            'attribute' => 'totalOcorrencia',
+            'label' => 'Número de Ocorrências',
+            'value'=> 2,
+        ],
+    ],
+]) ?>
+
+<div class="frequencias-index">
+    <h5 style="background-color: lightblue">
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            //'filterModel' => $searchModel,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+
+                //'idusuario',
+
+                [
+                    'attribute' => 'nomeusuario',
+                    'label' => "Nome",
+
+                ]
+                ,
+                ['attribute' => 'dataInicial',
+                    'value' => function ($model){
+                        return date('d-m-Y', strtotime($model->dataInicial));
+                    },
+                ],
+                ['attribute' => 'dataFinal',
+                    'value' => function ($model){
+                        return date('d-m-Y', strtotime($model->dataFinal));
+                    },
+                ],
+                /*
+                /////////////////////////////////////////////Somente para secretaria//////////////////////////////////////////////
+                ['class' => 'yii\grid\ActionColumn',
+                    'template'=>'{update} {delete}',
+                    'buttons'=>[
+                        'update' => function ($url, $model) {
+                            return Html::a('<span class="glyphicon glyphicon-pencil"></span>', ['update', 'id' => $model->id , "ano" => $_GET["ano"]], [
+                                'title' => Yii::t('yii', 'Editar Férias'),
+                            ]);
+                        },
+                        'delete' => function ($url, $model) {
+                            return Html::a('<span class="glyphicon glyphicon-remove"></span>', ['deletesecretaria', 'id' => $model->id, 'idUsuario' => $model->idusuario , 'ano'=>$_GET['ano'], 'prof' => $_GET["prof"],], [
+
+                                'data' => [
+                                    'confirm' => "Você realmente deseja excluir o registro dessas férias?",
+                                    'method' => 'post',
+                                ],
+
+                                'title' => Yii::t('yii', 'Remover Férias'),
+                            ]);
+                        }
+                    ]
+                ],*/
+            ],
+        ]); ?>
+    </h5>
+</div>
--- backend/views/frequencias/index.php
+++ backend/views/frequencias/index.php
@@ -2,22 +2,42 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use xj\bootbox\BootboxAsset;
+use app\models\Frequencias;
+BootboxAsset::register($this);
+BootboxAsset::registerWithOverride($this);
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\FrequenciasSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Frequências';
+$this->title = 'Minhas Frequências';
 $this->params['breadcrumbs'][] = $this->title;
+
 ?>
+
+<script type="text/javascript">
+
+    function anoSelecionado(){
+        var x = document.getElementById("comboBoxAno").value;
+
+        window.location="index.php?r=ferias/listar&ano="+x;
+
+    }
+
+</script>
+
+<p>
+    <?= Html::a('<span class="glyphicon glyphicon-arrow-left"></span> Voltar  ', ['site/index'], ['class' => 'btn btn-warning']) ?>
+    <?= Html::a('Registrar Frequências', ['create', "ano" => $_GET["ano"]], ['class' => 'btn btn-success']) ?>
+</p>
+
+
 <div class="frequencias-index">
 
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?= Html::a('Nova Frequência', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
@@ -25,14 +45,24 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
-            'idusuario',
+            //'id',
+            //'idusuario',
             'nomeusuario',
             'dataInicial',
             'dataFinal',
             // 'codigoOcorrencia',
 
-            ['class' => 'yii\grid\ActionColumn'],
+            ['class' => 'yii\grid\ActionColumn',
+                'template'=>'{view}',
+                'buttons'=>[
+                    'view' => function ($url, $model) {
+                        return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', ['detalhar',
+                            'id' => $model->id , 'ano' => $_GET["ano"], "prof" => 1], [
+                            'title' => Yii::t('yii', 'Visualizar Detalhes'),
+                        ]);
+                    }
+                ]
+            ],
         ],
     ]); ?>
 </div>
